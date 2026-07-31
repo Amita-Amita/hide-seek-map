3e4ff51ed93f067bc597f44de80da6d50e507780
Amita-Amita <ymartins1999@gmail.com>
2026-07-31 01:18:06 -0300

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Test(x: {depth: string}) {
+  const y:number = +x.depth;
+  const gray = y ^ (y >> 1);
+  let cor = "#";
+  for (let i = 0; i < 3; i++) {
+    if (gray&(1<<i)) {
+      cor = cor + 'FF';
+    } else {
+      cor = cor + '00';
+    }
+  }
+  return (
+    <div style={{color: cor, fontSize: "1.1em", lineHeight: "0.1em"}}>
+      Hello World!
+      { (y > 0) && 
+         (<Test depth={''+(y-1)}/>)
+       }
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {/* Edit <code>src/App.tsx</code> and save to reload. */}
+          <Test depth="7" />
         </p>
         <a
           className="App-link"
